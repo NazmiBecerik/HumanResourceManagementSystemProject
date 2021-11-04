042a6900abdf373cbdf8f9a506a0534f0a344770
Nazmi Becerik <nazmibecerik@outlook.com>
2021-11-04 11:34:51 +0300

    Api Katmanı Oluşturuldu

--- src/main/java/project/hrms/dataAccess/abstracts/EmployerDao.java
+++ src/main/java/project/hrms/dataAccess/abstracts/EmployerDao.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.hrms.entities.concretes.Employer;
 
 public interface EmployerDao extends JpaRepository <Employer,Integer>{
+    Employer findBy
 
 }
